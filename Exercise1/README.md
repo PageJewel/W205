538f5275eadf58a9af720e2a946a3a662bbd9360
Alyssa <alyssa.eisenberg@gmail.com>
2017-12-10 17:48:21 -0500

    Testing fixes

--- Exercise1/README.md
+++ Exercise1/README.md
@@ -2,10 +2,13 @@
 
 # Inital Setup
 1. Pull Exercise1 repository in EC2 instance (`git clone https://github.com/PageJewel/W205/Exercise1.git`).
-2. Create a directory Exercise1/data and place the unzipped data in that directory.
+2. Create a directory Exercise1/data - type `mkdir data` from Exercise1 folder.
+3. Move the flat files into Exercise1/data if you already have them. Otherwise:
+- In Exercise1/data, download the data by typing `wget -O Hospital_Revised_Flatfiles.zip "https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3\Dbinary&filename=Hospital_Revised_Flatfiles.zip"`
+- Unzip the downloaded data by typing `unzip Hospital_Revised_Flatfiles.zip`
 
 # Clean the data files and load into HDFS directory
-3. Type `su - w205`. This switchews from root to w205 user. Start Hadoop, postgres, and Hive in EC2 instance.
-4. Go to Exercise1/loading_and_modelling, and type `. load_data_lake.sh`. This processes the data files and loads them into the hdfs file structure.
+4. Start Hadoop, postgres, and Hive metastore in EC2 instance.
+5. Go to Exercise1/loading_and_modelling, and type `. load_data_lake.sh`. This processes the data files and loads them into the hdfs file structure. It also switches the user from root to w205 as it begins the hdfs steps.
 
 # Transform the data
--- Exercise1/loading_and_modelling/load_data_lake.sh
+++ Exercise1/loading_and_modelling/load_data_lake.sh
@@ -10,22 +10,25 @@ tail -n +2 "Timely and Effective Care - State.csv" > effective_care_state.csv
 tail -n +2 "Hospital General Information.csv" > hospital_info.csv
 tail -n +2 "hvbp_hcahps_05_28_2015.csv" > survey_responses.csv
 
+# Switch user
+su - w205
+
 # create hdfs folder structure
-hdfs dfs -mkdir /user/w205/hospital_compare
-hdfs dfs -mkdir /user/w205/hospital_compare/readmit_deaths_hospital
-hdfs dfs -mkdir /user/w205/hospital_compare/readmit_deaths_state
-hdfs dfs -mkdir /user/w205/hospital_compare/effective_care_hospital
-hdfs dfs -mkdir /user/w205/hospital_compare/effective_care_state
-hdfs dfs -mkdir /user/w205/hospital_compare/hospital_info
-hdfs dfs -mkdir /user/w205/hospital_compare/survey_responses
+#hdfs dfs -mkdir /user/w205/hospital_compare
+#hdfs dfs -mkdir /user/w205/hospital_compare/readmit_deaths_hospital
+#hdfs dfs -mkdir /user/w205/hospital_compare/readmit_deaths_state
+#hdfs dfs -mkdir /user/w205/hospital_compare/effective_care_hospital
+#hdfs dfs -mkdir /user/w205/hospital_compare/effective_care_state
+#hdfs dfs -mkdir /user/w205/hospital_compare/hospital_info
+#hdfs dfs -mkdir /user/w205/hospital_compare/survey_responses
 
 # Place processed files into hdfs structure
-hdfs dfs -put readmit_deaths_hospital.csv /user/w205/hospital_compare/readmit_deaths_hospital
-hdfs dfs -put readmit_deaths_state.csv /user/w205/hospital_compare/readmit_deaths_state
-hdfs dfs -put effective_care_hospital.csv /user/w205/hospital_compare/effective_care_hospital
-hdfs dfs -put effective_care_state.csv /user/w205/hospital_compare/effective_care_state
-hdfs dfs -put hospital_info.csv /user/w205/hospital_compare/hospital_info
-hdfs dfs -put survey_responses.csv /user/w205/hospital_compare/survey_responses
+#hdfs dfs -put readmit_deaths_hospital.csv /user/w205/hospital_compare/readmit_deaths_hospital
+#hdfs dfs -put readmit_deaths_state.csv /user/w205/hospital_compare/readmit_deaths_state
+#hdfs dfs -put effective_care_hospital.csv /user/w205/hospital_compare/effective_care_hospital
+#hdfs dfs -put effective_care_state.csv /user/w205/hospital_compare/effective_care_state
+#hdfs dfs -put hospital_info.csv /user/w205/hospital_compare/hospital_info
+#hdfs dfs -put survey_responses.csv /user/w205/hospital_compare/survey_responses
 
 # Switch back to initial directory
-cd ../loading_and_modelling
+#cd ../loading_and_modelling
