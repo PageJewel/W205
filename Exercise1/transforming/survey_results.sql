85a173ed7dcd1cdfb0994cff2078d93cdf85d426
Alyssa <alyssa.eisenberg@gmail.com>
2017-12-13 14:05:58 -0500

    Fixed bug and added survey results

--- Exercise1/transforming/survey_results.sql
+++ Exercise1/transforming/survey_results.sql
@@ -2,7 +2,7 @@ DROP TABLE survey_results;
 CREATE TABLE survey_results AS
 SELECT
   s.provider_number AS provider_id,
-  s.hcahps_base_score AS overall_rating
+  CAST(s.hcahps_base_score AS SMALLINT) AS overall_rating
 FROM survey_responses s
 INNER JOIN hospitals h
 	ON h.provider_id = s.provider_number
